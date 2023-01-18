01fd1cce3590f6f34e0237e319044f6b273771ec
mariananieton <rm94141@fiap.com.br>
2023-01-18 12:05:07 -0300

    Utilizando BufferedWriter para escrever um arquivo .csv com o numero de pacientes por procedimento

--- src/main/java/br/com/hospital/das/cruzes/job/OutputFirstReportJob.java
+++ src/main/java/br/com/hospital/das/cruzes/job/OutputFirstReportJob.java
@@ -3,19 +3,34 @@ package br.com.hospital.das.cruzes.job;
 import br.com.hospital.das.cruzes.dao.PacienteDao;
 import br.com.hospital.das.cruzes.model.Paciente;
 
+import java.io.*;
 import java.util.*;
 
 public class OutputFirstReportJob {
 
 	public static final String FILE_NAME = "BASE_HOSP_CRUZ_2023_REPORT1.csv";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		PacienteDao pacienteDao = new PacienteDao();
 		List<Paciente> pacientes = pacienteDao.consultarTodos();
 
 		Map<String, List<Paciente>> pacientesAgrupadosPorProcedimento = agrupaPacientesPorProcedimento(pacientes);
 		printaNumeroDePacientesPorProcedimento(pacientesAgrupadosPorProcedimento);
+
+		OutputStream outputStream = new FileOutputStream(FILE_NAME);
+		Writer writer = new OutputStreamWriter(outputStream);
+		BufferedWriter bufferedWriter = new BufferedWriter(writer);
+
+		String cabecalho = "procedimento;numeroDePacientes";
+
+		bufferedWriter.write(cabecalho);
+		bufferedWriter.newLine();
+		for (String procedimento : pacientesAgrupadosPorProcedimento.keySet()) {
+			bufferedWriter.write(procedimento + ";" + pacientesAgrupadosPorProcedimento.get(procedimento).size());
+			bufferedWriter.newLine();
+		}
+		bufferedWriter.close();
 	}
 
 	private static Map<String, List<Paciente>> agrupaPacientesPorProcedimento(List<Paciente> pacientes) {
